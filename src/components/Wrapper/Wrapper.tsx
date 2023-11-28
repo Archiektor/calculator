4916da2760c45dcf0c150d84e7fa1942c4f430f3
Nikki Odd <archiektor@gmail.com>
2023-11-28 20:35:39 +0100

    - all work - just refactor

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -2,21 +2,18 @@ import React, {useReducer, useState} from 'react';
 import './Wrapper.css';
 import {Screen} from '../Screen/Screen';
 import {
-    calculatorReducer, equalNumberOnClickAC, onNumberClickAC, onResetClickAC, plusNumberOnClickAC,
+    calculatorReducer, divideNumberOnClickAC, equalNumberOnClickAC, minusNumberOnClickAC, multiplyNumberOnClickAC, onNumberClickAC, onResetClickAC, plusNumberOnClickAC,
 } from '../../reducer/calculatorReducer';
 import {Button} from '../Button/Button';
-
+enum BtnClasses {
+    Usual = 'btn',
+    Middle = 'btn-middle',
+    Light = 'btn-light',
+    Dark = 'btn-dark'
+}
 export const Wrapper = () => {
-    const [screenInfoObj, dispatchScreenInfo] = useReducer(calculatorReducer, {screenInfo: 0, additionalScreenInfo: 0});
+    const [screenInfoObj, dispatchScreenInfo] = useReducer(calculatorReducer, {screenInfo: 0, additionalScreenInfo: 0, lastOperation: ''});
     const [isOperation, setIsOperation] = useState<boolean>(false);
-
-
-    enum BtnClasses {
-        Usual = 'btn',
-        Middle = 'btn-middle',
-        Light = 'btn-light',
-        Dark = 'btn-dark'
-    }
 
     const onNumberClick = (e: React.MouseEvent<HTMLButtonElement>) => {
         dispatchScreenInfo(onNumberClickAC(+e.currentTarget.innerHTML, isOperation));
@@ -27,11 +24,21 @@ export const Wrapper = () => {
         dispatchScreenInfo(onResetClickAC());
     }
 
+    //todo replace in reduce
     const actOnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
         setIsOperation(true);
         switch (e.currentTarget.innerHTML) {
             case '+':
                 dispatchScreenInfo(plusNumberOnClickAC(screenInfoObj.screenInfo));
+                break;
+            case '-':
+                dispatchScreenInfo(minusNumberOnClickAC(screenInfoObj.screenInfo));
+                break;
+            case '*':
+                dispatchScreenInfo(multiplyNumberOnClickAC(screenInfoObj.screenInfo));
+                break;
+            case '/':
+                dispatchScreenInfo(divideNumberOnClickAC(screenInfoObj.screenInfo));
                 break;
             case '=':
                 dispatchScreenInfo(equalNumberOnClickAC());
@@ -39,33 +46,35 @@ export const Wrapper = () => {
         }
     }
 
-    return (
+    type BtnProps = [number, string, BtnClasses, BtnClasses, (event: React.MouseEvent<HTMLButtonElement>) => void];
+    const arrOfBtnProps: BtnProps [] = [
+        [0, '/', BtnClasses.Usual, BtnClasses.Dark, actOnClick],
+        [1, '*', BtnClasses.Usual, BtnClasses.Dark, actOnClick],
+        [2, '-', BtnClasses.Usual, BtnClasses.Dark, actOnClick],
+        [3, '+', BtnClasses.Usual, BtnClasses.Dark, actOnClick],
+        [4, '9', BtnClasses.Usual, BtnClasses.Middle, onNumberClick],
+        [5, '8', BtnClasses.Usual, BtnClasses.Middle, onNumberClick],
+        [6, '7', BtnClasses.Usual, BtnClasses.Middle, onNumberClick],
+        [7, '6', BtnClasses.Usual, BtnClasses.Middle, onNumberClick],
+        [8, '5', BtnClasses.Usual, BtnClasses.Middle, onNumberClick],
+        [9, '4', BtnClasses.Usual, BtnClasses.Middle, onNumberClick],
+        [10, '3', BtnClasses.Usual, BtnClasses.Middle, onNumberClick],
+        [11, '2', BtnClasses.Usual, BtnClasses.Middle, onNumberClick],
+        [12, '1', BtnClasses.Usual, BtnClasses.Middle, onNumberClick],
+        [13, '0', BtnClasses.Usual, BtnClasses.Middle, onNumberClick],
+        [14, 'C', BtnClasses.Usual, BtnClasses.Dark, onResetClick],
+        [15, '=', BtnClasses.Usual, BtnClasses.Light, actOnClick],
+    ]
 
+    return (
         <div className={'wrapper'}>
             <Screen value={screenInfoObj}/>
             {/*todo wrap in useMemo*/}
             <div className={'btnWrapper'}>
-                <Button name={'0'} classes={`${BtnClasses.Usual} ${BtnClasses.Middle}`} callback={onNumberClick}/>
-                <Button name={'1'} classes={`${BtnClasses.Usual} ${BtnClasses.Middle}`} callback={onNumberClick}/>
-                <Button name={'2'} classes={`${BtnClasses.Usual} ${BtnClasses.Middle}`} callback={onNumberClick}/>
-                <Button name={'3'} classes={`${BtnClasses.Usual} ${BtnClasses.Middle}`} callback={onNumberClick}/>
-                <Button name={'4'} classes={`${BtnClasses.Usual} ${BtnClasses.Middle}`} callback={onNumberClick}/>
-                <Button name={'5'} classes={`${BtnClasses.Usual} ${BtnClasses.Middle}`} callback={onNumberClick}/>
-                <Button name={'6'} classes={`${BtnClasses.Usual} ${BtnClasses.Middle}`} callback={onNumberClick}/>
-                <Button name={'7'} classes={`${BtnClasses.Usual} ${BtnClasses.Middle}`} callback={onNumberClick}/>
-                <Button name={'+'} classes={`${BtnClasses.Usual} ${BtnClasses.Dark}`} callback={actOnClick}/>
-                <Button name={'-'} classes={`${BtnClasses.Usual} ${BtnClasses.Dark}`} callback={() => {
-                }}/>
-                <Button name={'*'} classes={`${BtnClasses.Usual} ${BtnClasses.Dark}`} callback={() => {
-                }}/>
-                <Button name={'/'} classes={`${BtnClasses.Usual} ${BtnClasses.Dark}`} callback={() => {
-                }}/>
-                <Button name={'^n'} classes={`${BtnClasses.Usual} ${BtnClasses.Dark}`} callback={() => {
-                }}/>
-                <Button name={'x²'} classes={`${BtnClasses.Usual} ${BtnClasses.Dark}`} callback={() => {
-                }}/>
-                <Button name={'C'} classes={`${BtnClasses.Usual} ${BtnClasses.Dark}`} callback={onResetClick}/>
-                <Button name={'='} classes={`${BtnClasses.Usual} ${BtnClasses.Light}`} callback={actOnClick}/>
+                {arrOfBtnProps.map(el => {
+                    //todo object not array
+                    return <Button key={el[0]} name={el[1]} classes={`${el[2]} ${el[3]}`} callback={el[4]}/>
+                })}
             </div>
         </div>
     );
--- src/reducer/calculatorReducer.ts
+++ src/reducer/calculatorReducer.ts
@@ -1,6 +1,14 @@
+enum Operations {
+    Plus = '+',
+    Minus = '-',
+    Multiply = '*',
+    Divide = '/'
+}
+
 export type CalcStateType = {
     screenInfo: number,
     additionalScreenInfo: number,
+    lastOperation: string
 }
 
 type ActionObjType = {
@@ -11,6 +19,7 @@ type ActionObjType = {
     };
 }
 
+
 const makeCheckAndReturn = (stateObj: CalcStateType, actionObj: ActionObjType): CalcStateType => {
     return !stateObj.screenInfo ? {...stateObj, screenInfo: actionObj['payload']['num']} :
         {
@@ -19,53 +28,71 @@ const makeCheckAndReturn = (stateObj: CalcStateType, actionObj: ActionObjType): 
         }
 }
 
-// TODO Why it doesn't work ???
-// const makeCheckAndReturn2 = (stateObj: CalcStateType, actionObj: ActionsType): CalcStateType => {
-//     return !stateObj.screenInfo ? {...stateObj, screenInfo: actionObj['payload']['num']} :
-//         {
-//             ...stateObj,
-//             screenInfo: Number(stateObj['screenInfo'].toString() + actionObj['payload']['num'])
-//         }
-// }
-
 export const calculatorReducer = (state: CalcStateType, action: ActionsType): CalcStateType => {
     switch (action.type) {
         case 'NUMBER-CLICK':
-            //debugger;
+            //todo refactor BLOCK SCOPE {}
             if (action.payload.isOperation) {
                 state.screenInfo = 0;
                 return makeCheckAndReturn(state, action);
-                // !state.screenInfo ? {...state, screenInfo: action['payload']['num']} :
-                //     {
-                //         ...state,
-                //         screenInfo: Number(state['screenInfo'].toString() + action['payload']['num'])
-                //     }
             } else {
                 return makeCheckAndReturn(state, action);
-                // !state.screenInfo ? {...state, screenInfo: action['payload']['num']} :
-                //     {
-                //         ...state,
-                //         screenInfo: Number(state['screenInfo'].toString() + action['payload']['num'])
-                //     }
             }
 
         case 'RESET-CLICK':
-            return {...state, screenInfo: action['payload']['value'], additionalScreenInfo: action['payload']['value']};
+            return {...state, screenInfo: action.payload.value, additionalScreenInfo: action['payload']['value']};
         case "ADD-NUMBER-ON-CLICK":
-            return {...state, additionalScreenInfo: action['payload']['num']};
-        case "EQUAL-ON-CLICK":
-            return {...state, screenInfo: state.screenInfo + state.additionalScreenInfo};
+            state.lastOperation = Operations.Plus;
+            return state.additionalScreenInfo === 0 ? {...state, additionalScreenInfo: action['payload']['num']} :
+                {...state, additionalScreenInfo: state.additionalScreenInfo + action['payload']['num']}
+        case "MINUS-NUMBER-ON-CLICK":
+            state.lastOperation = Operations.Minus;
+            return state.additionalScreenInfo === 0 ? {...state, additionalScreenInfo: action['payload']['num']} :
+                {...state, additionalScreenInfo: state.additionalScreenInfo - action['payload']['num']}
+        case "MULTIPLY-NUMBER-ON-CLICK":
+            state.lastOperation = Operations.Multiply;
+            return state.additionalScreenInfo === 0 ? {...state, additionalScreenInfo: action['payload']['num']} :
+                {...state, additionalScreenInfo: state.additionalScreenInfo * action['payload']['num']}
+        case "DIVIDE-NUMBER-ON-CLICK":
+            state.lastOperation = Operations.Divide;
+            return state.additionalScreenInfo === 0 ? {...state, additionalScreenInfo: action['payload']['num']} :
+                {...state, additionalScreenInfo: state.additionalScreenInfo / action['payload']['num']}
+        case "EQUAL-ON-CLICK": {
+            if (state.lastOperation === '+') {
+                state = {...state, screenInfo: state.screenInfo + state.additionalScreenInfo, additionalScreenInfo: 0}
+            }
+            if (state.lastOperation === '-') {
+                state = {...state, screenInfo: state.screenInfo - state.additionalScreenInfo, additionalScreenInfo: 0}
+            }
+            if (state.lastOperation === '*') {
+                state = {...state, screenInfo: state.screenInfo * state.additionalScreenInfo, additionalScreenInfo: 0}
+            }
+            if (state.lastOperation === '/') {
+                state = {...state, screenInfo: state.screenInfo / state.additionalScreenInfo, additionalScreenInfo: 0}
+            }
+            return {...state}
+        }
         default :
             return state;
     }
 }
 
-type ActionsType = onNumberClickType | onResetClickType | addNumberOnClickACType | equalNumberOnClickACType;
+type ActionsType =
+    onNumberClickType
+    | onResetClickType
+    | addNumberOnClickACType
+    | equalNumberOnClickACType
+    | minusNumberOnClickACType
+    | multiplyNumberOnClickACType
+    | divideNumberOnClickACType;
 
 type onNumberClickType = ReturnType<typeof onNumberClickAC>
 type onResetClickType = ReturnType<typeof onResetClickAC>
 type addNumberOnClickACType = ReturnType<typeof plusNumberOnClickAC>
 type equalNumberOnClickACType = ReturnType<typeof equalNumberOnClickAC>
+type minusNumberOnClickACType = ReturnType<typeof minusNumberOnClickAC>
+type multiplyNumberOnClickACType = ReturnType<typeof multiplyNumberOnClickAC>
+type divideNumberOnClickACType = ReturnType<typeof divideNumberOnClickAC>
 
 export const onNumberClickAC = (num: number, isOperation: boolean) => {
     return {
@@ -95,8 +122,36 @@ export const plusNumberOnClickAC = (num: number) => {
     } as const
 }
 
+export const minusNumberOnClickAC = (num: number) => {
+    return {
+        type: 'MINUS-NUMBER-ON-CLICK',
+        payload: {
+            num,
+        }
+    } as const
+}
+
+export const multiplyNumberOnClickAC = (num: number) => {
+    return {
+        type: 'MULTIPLY-NUMBER-ON-CLICK',
+        payload: {
+            num,
+        }
+    } as const
+}
+
+export const divideNumberOnClickAC = (num: number) => {
+    return {
+        type: 'DIVIDE-NUMBER-ON-CLICK',
+        payload: {
+            num,
+        }
+    } as const
+}
+
 export const equalNumberOnClickAC = () => {
     return {
         type: 'EQUAL-ON-CLICK',
+        payload: {}
     } as const
 }
